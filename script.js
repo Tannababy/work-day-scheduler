0f46efc14e1102a4782135d997ced7cb5440859f
Lotanna <tannaloamobi@gmail.com>
2023-12-22 21:04:11 +0000

    added content to html,css and js file

---
+++ script.js
@@ -0,0 +1,18 @@
+//for loop
+//create div
+//create p
+//create textarea
+//create button
+
+//append p, textarea, buttotn to div
+//append div to html container
+
+//loop through each one of the textarea, get the time of that timeblock, and compare it to the current time(dayjs)
+//jquery .each
+
+array.forEach((element) => {
+  //get time of timeblock
+  //compare: if statement
+  //timeblock < currentTime - add class past. Hint: $(this)
+  //do same thing for present, and future
+});
