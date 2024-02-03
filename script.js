2c2c4b86126e029a23ace99e6b4f8e19fb716420
Lotanna <tannaloamobi@gmail.com>
2024-02-03 02:40:33 +0000

    added logic to updateColors function

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 $(document).ready(function () {
   // Display current day
-  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  $("#currentDay").text(dayjs().format("DD, MMMM YYYY"));
 
-  // Generate time blocks
-  for (let hour = 1; hour <= 12; hour++) {
-      $(".container").append(`
-          <div class="time-block" id="hour-${hour}">
-              <p>${hour}</p>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
-              <button><i class="fas fa-save"></i></button>
-          </div>
-      `);
+  // Function to update colours based on past, present and future
+  function updateColors() {
+    //loop through each time block
+    $(".time-block").each(function () {
+      // get the hour of the time block
+      const hourBlock = parseInt($(this).attr("id").split("-")[1]);
+
+      //compare with the current hour
+      if (hourBlock < currentHour) {
+        //past
+        $(this).removeClass("presetn future").addClass("past");
+      } else if (blockHour === currentHour) {
+        // Present
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        // Future
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
   }
+
+  // Set interval to update colors every minute
+  setInterval(updateColors, 60000);
+
+  // Initial color update
+  updateColors();
+
+  // Event listener for save button
+  $(".saveBtn").on("click", function () {
+    // Your logic to save events to local storage
+  });
 });
