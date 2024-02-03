124802fc9e3a7bc847728b0222960ae715383627
Lotanna <tannaloamobi@gmail.com>
2024-02-03 02:00:37 +0000

    added javacript to script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-//for loop
-//create div
-//create p
-//create textarea
-//create button
+$(document).ready(function () {
+  // Display current day
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
-//append p, textarea, buttotn to div
-//append div to html container
-
-//loop through each one of the textarea, get the time of that timeblock, and compare it to the current time(dayjs)
-//jquery .each
-
-array.forEach((element) => {
-  //get time of timeblock
-  //compare: if statement
-  //timeblock < currentTime - add class past. Hint: $(this)
-  //do same thing for present, and future
+  // Generate time blocks
+  for (let hour = 1; hour <= 12; hour++) {
+      $(".container").append(`
+          <div class="time-block" id="hour-${hour}">
+              <p>${hour}</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <button><i class="fas fa-save"></i></button>
+          </div>
+      `);
+  }
 });
